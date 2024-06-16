ca990b01afc2184ebfa7f18e9712fadb664760a6
bayramkzk <48856944+bayramkzk@users.noreply.github.com>
2024-06-17 00:56:25 +0300

    Include cursor, color scheme adn gtk theme options in autotheme.sh

--- scripts/autotheme.sh
+++ scripts/autotheme.sh
@@ -6,23 +6,29 @@ case "$1" in
 	*) echo "Usage: $0 [-l|--light|-d|--dark]"; exit 0;;
 esac
 
-FONT_NAME="Fira Sans Regular 11"
+FONT_NAME="Fira Sans 10"
 MONOSPACE_FONT_NAME="Iosevka Nerd Font Mono Medium 11"
 CURSOR_THEME="elementary"
+CURSOR_SIZE=24
 
 if [[ $THEME = "light" ]]; then
 	GTK_THEME="adw-gtk3"
 	ICON_THEME="Papirus-Light"
 	DAY_NIGHT="day"
+	LIGHT_DARK="light"
 else
 	GTK_THEME="adw-gtk3-dark"
 	ICON_THEME="Papirus-Dark"
 	DAY_NIGHT="night"
+	LIGHT_DARK="dark"
 fi
 
+gsettings set org.gnome.desktop.interface color-scheme "$LIGHT_DARK"
 gsettings set org.gnome.desktop.interface gtk-theme "$GTK_THEME"
 gsettings set org.gnome.desktop.interface icon-theme "$ICON_THEME"
 gsettings set org.gnome.desktop.interface cursor-theme "$CURSOR_THEME"
+gsettings set org.gnome.desktop.interface cursor-size "$CURSOR_SIZE"
+gsettings set org.gnome.desktop.wm.preferences theme "$GTK_THEME"
 
 gsettings set org.gnome.desktop.interface document-font-name "$FONT_NAME"
 gsettings set org.gnome.desktop.interface font-name "$FONT_NAME"
